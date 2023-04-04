fb7d8a59f541115e0d4cc5e56b98bc38beaad0bf
Davidsonjose <davidsonjose313@gmail.com>
2023-04-04 18:29:24 +0100

    added certain changes

--- src/pages/auth/signup/index.tsx
+++ src/pages/auth/signup/index.tsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useForm } from 'react-hook-form';
 import Input from '@/components/form-control/input';
-import { LOGO } from '@/assets';
 import axios from 'axios';
 import API_URL from '@/config';
 import { useDispatch, useSelector } from 'react-redux';
